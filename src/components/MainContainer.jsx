18c8c1a4f5920a766d0656bd5eff8101b9f81961
Amit Verma <amitjsr8786@gmail.com>
2025-02-02 23:41:00 +0530

    addTrailer movie code

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -7,14 +7,14 @@ const MainContainer=()=>{
     //early return
     if(!movies) return;
     const mainMovie=movies[0]
-    console.log(mainMovie)
+    console.log("Main Movie",mainMovie)
 
-    const {original_title,overview}=mainMovie
+    const {original_title,overview,id}=mainMovie
     return(
         <div>
             <VideoTitle title={original_title} overview={overview}/>
         
-            <VideoBackground/>
+            <VideoBackground movieId={id}/>
 
         </div>
     )
--- src/util/movieSlice.js
+++ src/util/movieSlice.js
@@ -4,15 +4,18 @@ const movieSlice=createSlice({
     name:'movies',
     initialState:{
         nowPlayingMovies:null,
+        trailerVideos:null,
     },
     reducers:{
         addNowPlayingMovies:(state,action)=>{
             state.nowPlayingMovies=action.payload
-
+        },
+        addTrailerVideo:(state,action)=>{
+            state.trailerVideos=action.payload
         }
     }
 })
 
-export const {addNowPlayingMovies}=movieSlice.actions;
+export const {addNowPlayingMovies,addTrailerVideo}=movieSlice.actions;
 
 export default movieSlice.reducer;
